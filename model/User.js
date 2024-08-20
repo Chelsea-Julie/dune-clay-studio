3f652afac68ab81c2c4eae1acc732cb4f6f6c741
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-08-20 10:49:51 +0200

    productController

---
+++ model/User.js
@@ -0,0 +1,179 @@
+import { connection as db} from '../config/index.js'
+import {compare, hash} from 'bcrypt'
+
+class Users {
+    static fetchUsers(req, res) {
+        try {
+            const strQry = `
+            SELECT * 
+            FROM Users;`
+            db.query(strQry, (err, results) => {
+                //`Unable to fetch all users`
+                if (err) throw new Error (err.message);
+                res.json({
+                    status: res.statusCode,
+                    results
+                })
+            })
+        } catch (e) {
+            res.json({
+                status: 404,
+                message: e.message 
+            })
+        }
+        
+    }
+
+    static fetchOneUser(req, res) {
+        try {
+            const strQry = `
+            SELECT * 
+            FROM Users;`
+            db.query(strQry, (err, results) => {
+                if (err) throw new Error (`Unable to fetch all users`);
+                res.json({
+                    status: res.statusCode,
+                    results
+                })
+            })
+        } catch (e) {
+            res.json({
+                status: 404,
+                message: e.message 
+            })
+        }
+    }
+
+    static async registerUser(){
+        try {
+            let data = req.body
+            data.pwd = await hash(data.pwd,12)
+                // payload
+                let user = {
+                    emailAdd: data.emailAdd,
+                    pwd: data.pwd
+                }
+                let strQry = `
+                INSERT INTO Users 
+                 SET ?; 
+                `
+                db.query(strQry, [data], (err) => {
+                    if (err) {
+                        res.json({
+                            status:res.statusCode,
+                            msg: 'This email address already exists'
+                        })
+                    }   else {
+                        const token = createToken(user)
+                        res.json({
+                            token,
+                            msg:'You are now registered'
+                        })
+                    }
+                })
+            
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    static async updated() {
+        try {
+            let data = req.body
+            if (data.pwd) {
+                data.pwd = await hash(data.pwd, 12)
+            }
+            const strQry = `
+            UPDATE Users
+            SET ?
+            WHERE underID = ${req.params.id}
+            `
+            db.query(strQry, [data], (err) => {
+    
+                if (err) throw new Error (`Unable to update user}`);
+                res.json({
+                    status: res.statusCode,
+                    msg: 'User updated successfully'
+                })
+            })
+        } catch (e) {
+            res.json({
+                staus:400,
+                msg:e.message
+            })
+        }
+    }
+
+    static deleteUser() {
+        try {
+            const strQry = `
+            DELETE FROM Users WHERE underID = ${req.params.id};
+            `
+            db.query(strQry, (err) => {
+                if (err) throw new Error('Error deleting')
+                    res.json({
+                status: res.statusCode,
+                    msg: 'User deleted successfully'
+                })
+            })
+        } catch (e) {
+            res.json({
+                status: 404,
+                msg: e.message
+            })
+        }
+    }
+
+    static loginUser() {
+        try {
+            const { emailAdd, pwd} = req.body
+            const strQry = `
+            SELECT *
+            FROM Users 
+            WHERE emailAdd = '${emailAdd}'
+            `
+            db.query(strQry, async (err, result) => {
+                if (err) throw new Error('To login, please try again')
+                if (!result?.length) {
+                    res.json(
+                        {
+                            status: 401,
+                            msg: 'Invalid email'
+                        }
+                    )
+                } else {
+                    const isValidPass = await compare
+                    (pwd, result[0].pwd)
+                    if (isValidPass) {
+                        const token = createToken({
+                            emailAdd,
+                            pwd
+                        })
+                        res.json({
+                            status: res.statusCode,
+                            token,
+                            result: result[0]
+                        })
+                    } else {
+                        res.json(
+                            {
+                                status: 401,
+                                msg: 'Invalid password'
+                            }
+                        )
+                    }
+                } 
+            })
+        } catch (e) {
+            
+        }
+    }
+
+    
+
+
+}
+
+export{
+    Users
+}
