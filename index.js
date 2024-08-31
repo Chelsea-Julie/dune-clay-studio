a0d63378d4bbbd752061a431fa27bb0556f1278d
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-08-31 12:00:04 +0200

    cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 // app.use(cors())
 
-app.use(cors({ origin: 'http://localhost:8080'}));
+// app.use(cors({ origin: 'http://localhost:8080'}));
 
 app.use('/users', userRouter)
 app.use('/products', productRouter)
